a47879934309c5803aaa5c7b90de03c70692bcb0
張哲菘 <chang-che-song@zhangzhesongdeMacBook-Air.local>
2024-01-05 14:48:23 +0800

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,10 @@ export default ({ data }) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/my-first-post.md/" } }
+    ) {
       edges {
         node {
           id
